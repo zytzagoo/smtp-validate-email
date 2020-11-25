efdecbe0d6360df97ec31e47d0002d49d7019818
zyt <zytzagoo@gmail.com>
2020-11-25 20:59:52 +0100

    devdeps: shellcheck stop-server.sh

--- bin/stop-server.sh
+++ bin/stop-server.sh
@@ -6,14 +6,14 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 stop_server () {
-    if [ -e $1 ]; then
-        PID=$(cat $1);
-        echo PID=$PID;
+    if [ -e "$1" ]; then
+        PID=$(cat "$1");
+        echo PID="$PID";
         # Killing juts the PID is not enough sometimes, nodejs server still lingers on (at least on Windows)
-        if [ ! -z "$PID" ]; then
-            kill $PID || true;
+        if [ -n "$PID" ]; then
+            kill "$PID" || true;
         fi;
-        rm -rf $1 || true;
+        rm -rf "$1" || true;
     fi;
 }
 
